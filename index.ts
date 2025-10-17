4e20642e65f775ab62902843d4f7deb146a0fc9d
key8888 <aottware@gmail.com>
2025-10-17 14:32:54 +0900

    change words to 30

--- index.ts
+++ index.ts
@@ -322,7 +322,7 @@ class TypingHandler extends Handler {
 
 
         // wordsの0~300番目をランダムに5単語のみ抽出
-        const randomWords = words.slice(0, 300).sort(() => 0.5 - Math.random()).slice(0, 5);
+        const randomWords = words.slice(0, 300).sort(() => 0.5 - Math.random()).slice(0, 30);
         console.log(randomWords);
 
         // ユーザーID（ログイン必須にしたい場合）
